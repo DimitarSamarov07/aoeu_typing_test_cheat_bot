a242808d3dff214e69d367994abe7b36a247f77b
Dimitar Samarov <samarovdimitar@gmail.com>
2021-08-10 13:20:08 +0300

    Handle if the user manually clicks the privacy agreement button

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from selenium import webdriver
 
+# Initialize the driver and get the page
+from selenium.common.exceptions import NoSuchElementException
+
 driver = webdriver.Firefox()
 driver.get("https://typing-speed-test.aoeu.eu/")
 
-agreement_button = driver.find_element_by_xpath("//button[text()='AGREE']")
-agreement_button.click()
+try:
+    agreement_button = driver.find_element_by_xpath("//button[text()='AGREE']")
+    agreement_button.click()
+
+except NoSuchElementException:
+    print("No agreement button was found. If this is a bug, report it.\n"
+          "For now if there's a button, you can click it yourself")
+
 input_el = driver.find_element_by_id("input")
 span_arr_of_words = driver.find_element_by_id("words").find_elements_by_tag_name("span")
 
