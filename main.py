1460fff4115ed60d9d6a241d81d1ac0dd680d51b
Dimitar Samarov <samarovdimitar@gmail.com>
2021-08-10 13:11:01 +0300

    Further optimize the code by getting the values of all the elements beforehand (twice as fast now)

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ agreement_button.click()
 input_el = driver.find_element_by_id("input")
 span_arr_of_words = driver.find_element_by_id("words").find_elements_by_tag_name("span")
 
+# Get the texts before the actual typing test so it's faster
+plain_words_arr = []
 for word in span_arr_of_words:
-    input_el.send_keys(word.get_attribute("innerHTML") + " ")
+    plain_words_arr.append(word.get_attribute("innerHTML") + " ")
+
+for index, word in enumerate(span_arr_of_words):
+    input_el.send_keys(plain_words_arr[index])
